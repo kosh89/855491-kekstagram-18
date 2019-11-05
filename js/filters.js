f3f8d3f24637f60d427062098ad528bf57e2c898
Sergey Vasilyev <kosh.goo@gmail.com>
2019-11-05 18:32:45 +0300

    moved functions out of `filters` namespace

--- js/filters.js
+++ js/filters.js
@@ -11,33 +11,39 @@
     newActiveButton.classList.add(ACTIVE_BUTTON_CLASS);
   };
 
-  window.filters = {
-    onPopularButtonClick: function (picturesArray, button) {
-      window.updatePictures(picturesArray);
-      currentButtonBacklight(button);
-    },
+  var showPopularPictures = function (picturesArray, button) {
+    window.updatePictures(picturesArray);
+    currentButtonBacklight(button);
+  };
 
-    onRandomButtonClick: function (picturesArray, button) {
-      var picturesArrayCopy = window.utils.cloneArray(picturesArray);
-      var arrayOfRandomPictures = [];
+  var showRandomPictures = function (picturesArray, button) {
+    var picturesArrayCopy = window.utils.cloneArray(picturesArray);
+    var arrayOfRandomPictures = [];
 
-      for (var i = 0; i < RANDOM_ARRAY_LENGTH; i++) {
-        var index = window.utils.getRandomInt(0, picturesArrayCopy.length - 1);
-        arrayOfRandomPictures.push(picturesArrayCopy.splice(index, 1)[0]);
-      }
-
-      window.updatePictures(arrayOfRandomPictures);
-      currentButtonBacklight(button);
-    },
-
-    onDiscussedButtonClick: function (picturesArray, button) {
-      var arrayOfDiscussedPictures = picturesArray;
-      arrayOfDiscussedPictures.sort(function (prevPic, nextPic) {
-        return nextPic.comments.length - prevPic.comments.length;
-      });
-
-      window.updatePictures(arrayOfDiscussedPictures);
-      currentButtonBacklight(button);
+    for (var i = 0; i < RANDOM_ARRAY_LENGTH; i++) {
+      var index = window.utils.getRandomInt(0, picturesArrayCopy.length - 1);
+      arrayOfRandomPictures.push(picturesArrayCopy.splice(index, 1)[0]);
     }
+
+    window.updatePictures(arrayOfRandomPictures);
+    currentButtonBacklight(button);
+  };
+
+  var showDiscussedPictures = function (picturesArray, button) {
+    var arrayOfDiscussedPictures = picturesArray;
+    arrayOfDiscussedPictures.sort(function (prevPic, nextPic) {
+      return nextPic.comments.length - prevPic.comments.length;
+    });
+
+    window.updatePictures(arrayOfDiscussedPictures);
+    currentButtonBacklight(button);
+  };
+
+  window.filters = {
+    onPopularButtonClick: showPopularPictures,
+
+    onRandomButtonClick: showRandomPictures,
+
+    onDiscussedButtonClick: showDiscussedPictures
   };
 })();
