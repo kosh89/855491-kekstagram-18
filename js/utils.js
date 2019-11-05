3f4fc9f5d3e5d78073b9f60926b6716b1added7f
Sergey Vasilyev <kosh.goo@gmail.com>
2019-11-05 18:18:57 +0300

    added debounce and cloneArray functions

--- js/utils.js
+++ js/utils.js
@@ -3,8 +3,28 @@
 (function () {
   window.utils = {
     ESC_KEYCODE: 27,
+
     getRandomInt: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    cloneArray: function (array) {
+      var newArray = JSON.parse(JSON.stringify(array));
+      return newArray;
+    },
+
+    debounce: function (cb, time) {
+      var lastTimeout = null;
+
+      return function () {
+        var parameters = arguments;
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        lastTimeout = window.setTimeout(function () {
+          cb.apply(null, parameters);
+        }, time);
+      };
     }
   };
 })();
